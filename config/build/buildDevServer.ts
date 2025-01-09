9a5228715a578409e6f675ccce2506aa24d50e29
usmonzo <turaef.usmon@gmail.com>
2025-01-09 10:53:10 +0500

    add: additional webpack configs;

---
+++ config/build/buildDevServer.ts
@@ -0,0 +1,11 @@
+import type { Configuration as DevServerConfiguration } from 'webpack-dev-server'
+import { BuildOptions } from './types/config'
+
+export function buildDevServer(options: BuildOptions): DevServerConfiguration {
+	const { port } = options
+	return {
+		port: port,
+		open: true,
+		historyApiFallback: true,
+	}
+}
