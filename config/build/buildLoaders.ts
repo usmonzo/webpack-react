039c0d0d1297885ee1059d25bdc92f9fb9336465
usmonzo <turaef.usmon@gmail.com>
2025-01-20 10:09:16 +0500

    add: i18next extractor plugin && add babel;

--- config/build/buildLoaders.ts
+++ config/build/buildLoaders.ts
@@ -16,6 +16,26 @@ export function buildLoaders({ isDev }: BuildOptions): webpack.RuleSetRule[] {
 			},
 		],
 	}
+	const babelLoader = {
+		test: /\.(js|jsx|ts|tsx)$/,
+		exclude: /node_modules/,
+		use: {
+			loader: 'babel-loader',
+			options: {
+				presets: ['@babel/preset-env'],
+				plugins: [
+					[
+						'i18next-extract',
+						{
+							nsSeparator: '~',
+							locales: ['ru', 'en'],
+							ketAsDefaultValue: true,
+						},
+					],
+				],
+			},
+		},
+	}
 	const svgLoader = {
 		test: /\.svg$/,
 		use: [
@@ -39,9 +59,14 @@ export function buildLoaders({ isDev }: BuildOptions): webpack.RuleSetRule[] {
 					},
 				},
 			},
-			'sass-loader',
+			{
+				loader: 'sass-loader',
+				options: {
+					api: 'modern',
+				},
+			},
 		],
 	}
 
-	return [typescriptLoader, cssLoader, svgLoader,fileLoader]
+	return [babelLoader, typescriptLoader, cssLoader, svgLoader, fileLoader]
 }
--- src/shared/config/i18n/i18n.ts
+++ src/shared/config/i18n/i18n.ts
@@ -1,22 +1,18 @@
-import i18n from 'i18next';
-import { initReactI18next } from 'react-i18next';
-
-import Backend from 'i18next-http-backend';
-import LanguageDetector from 'i18next-browser-languagedetector';
+import i18n from 'i18next'
+import { initReactI18next } from 'react-i18next'
 
 i18n
-	.use(Backend)
-	.use(LanguageDetector)
+	// .use(Backend)
+	// .use(LanguageDetector)
 	.use(initReactI18next)
 
 	.init({
-		fallbackLng: 'ru',
+		fallbackLng: 'en',
 		debug: __IS_DEV__,
 
 		interpolation: {
 			escapeValue: false, // not needed for react as it escapes by default
 		},
-	});
+	})
 
-
-export default i18n;
+export default i18n
